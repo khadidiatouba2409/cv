83f02e5f089f995c8c14a5ab49a6be25cc355d8e
khadidiatouba2409 <khadidiatouba2409@gmail.com>
2026-02-27 16:11:17 +0100

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+
+
+
+
+with st.sidebar:
+    st.markdown(" 📞 CONTACTS")
+    st.write("📧Email : khadidiatouba2409@gmail.com")
+    st.write("📍Adresse : Mermoz / Sacré-Cœur 1")
+
+    st.divider()
+
+    st.markdown(" 🎓 FORMATIONS")
+    st.markdown("""
+    - BTS en Géomatique — 2025/2026  
+    - Baccalauréat S2 — 2023/2024
+    """)
+
+    st.divider()  
+
+    st.markdown(" 💻 LOGICIELS")
+    st.markdown("""
+    - QGIS  
+    - ArcGIS  
+    - PostgreSQL / PostGIS  
+    - AutoCAD  
+    - Erdas 
+    - Excel
+    """)
+
+
+ 
+
+
+st.header("KHADIDIATOU BA")
+st.header("Technicienne Supérieure en Géomatique")
+
+st.subheader("👤À propos de moi")
+st.write("""
+Passionnée par la géomatique, je m’intéresse particulièrement à l’analyse des données géospatiales et à l’exploitation des systèmes d’information géographique pour appuyer la prise de décision dans les projets territoriaux.
+""")
+
+st.subheader("💼 Expérience professionnelle")
+st.markdown("""
+Stage : Direction de la Gestion et  de la Planification  des Ressources en Eau (DEGPRE) durant 1 mois  
+ - Participation au traitement et à l’analyse des données liées aux ressources en eau  
+ - Contribution à l’organisation des données géospatiales  
+""")
+
+st.subheader("🖥Compétences Techniques")
+st.markdown("""
+-Numériser et structurer des données géographiques
+
+-Géoréférencer des cartes et des images satellites
+
+-Traiter et analyser des données géospatiales
+
+-Produire des cartes adaptées aux besoins d’un projet
+
+
+-Exploiter les outils SIG pour l’aide à la décision
+-Concevoir et organiser des bases de données spatiales 
+""")
+    
+
+   
